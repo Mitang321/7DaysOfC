d23258a0a7a42da6b4ecdf1e608fbd28f708bfb8
Mitang Hindocha <85874083+Mitang321@users.noreply.github.com>
2023-04-11 16:54:47 +0530

    Add files via upload

--- day5/array.c
+++ day5/array.c
@@ -10,3 +10,10 @@ printf("Total Price 1 : %f\n", price[0]+ (0.18* price[0]));
 printf("Total Price 2 : %f\n", price[1]+ (0.18* price[1]));
 printf("Total Price 3 : %f\n", price[2]+ (0.18* price[2]));
 }
+
+
+
+
+
+
+
